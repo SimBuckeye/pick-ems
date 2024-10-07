2d944db5b1394029e95ed8052a6015dac979f40b
SimBuckeye <leo.steinkerchner@gmail.com>
2024-10-07 10:07:00 -0400

    feat(auth): redirect to destination after login

--- pick-ems/src/app/feature/auth/login-page.component.ts
+++ pick-ems/src/app/feature/auth/login-page.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
 import { MessageService } from "primeng/api";
@@ -48,16 +48,18 @@ import { MessageService } from "primeng/api";
     
     `,
 })
-export default class LoginPageComponent {
+export default class LoginPageComponent implements OnInit {
     private readonly supabase = inject(SupabaseClient);
     private readonly formBuilder = inject(FormBuilder);
-    private readonly router = inject(Router);
     private readonly messageService = inject(MessageService);
+    returnUrl: string = "/";
 
     readonly form = this.formBuilder.nonNullable.group({
         username: ['', Validators.required],
         password: ['', Validators.required],
     });
+
+    constructor(private route: ActivatedRoute, private router: Router){}
 
     onSubmit(): void {
         const { username, password } = this.form.getRawValue();
@@ -68,8 +70,14 @@ export default class LoginPageComponent {
             if(response.error){
                 this.messageService.add({severity: 'error', detail: response.error.message})
             } else {
-                this.router.navigate(['/']);
+                this.router.navigateByUrl(this.returnUrl);
             }
         });
+    }
+
+    ngOnInit() {
+        this.route.queryParams.subscribe((params) => {
+            this.returnUrl = params['returnUrl'] || '/';
+        })
     }
 }
